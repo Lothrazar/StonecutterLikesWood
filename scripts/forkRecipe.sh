d26246da70e7c52893c504a5d23cca80c4d6c25f
lothrazar <samson.bassett@gmail.com>
2024-07-13 12:41:46 -0700

    script to copy and sed files

--- scripts/forkRecipe.sh
+++ scripts/forkRecipe.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
-
 [[ -z "$1" ]] && { echo "wood is empty for example: spruce" ; exit 1; }
 
-
-declare -a arr=("boat" "button" "door" "fence" "gate" "hanging_sign" "log_sticks" "plate" "sign" "slab" "stairs" "stripped_log" "stripped_wood" "trapdoor" "wood_log")
-
+# assumes oak recipes are made, copies them to the destination wood type
 
 wood=$1
-
 
 modid="stonecutterlikeswood"
 folder="src/main/resources/data/${modid}/recipes"
 
+# make dir if it does not exist yet
+mkdir -p "${folder}"/"${wood}"/
 
-
+# list all valid file recipes
+# TODO: improvement could be to loop on all files in /oak/ folder 
+declare -a arr=("boat" "button" "door" "fence" "gate" "hanging_sign" "log_sticks" "plate" "sign" "slab" "stairs" "stripped_log" "stripped_wood" "trapdoor" "wood_log")
 arraylength=${#arr[@]}
-## now loop through the above array
-# use for loop to read all values and indexes
-
 
 for (( i=0; i<${arraylength}; i++ ));
 do
-
-   recipe="${arr[$i]}"
-  # echo "$color"
+  recipe="${arr[$i]}"
+  
+  # Copy the source oak recipe to the new input wood type
    
-   # blockstate      
-	cp "${folder}"/oak/"${recipe}".json "${folder}"/"${wood}"/$recipe.json
+  cp -rf "${folder}"/oak/"${recipe}".json "${folder}"/"${wood}"/$recipe.json
 	
-	sed -i -e "s/oak/${wood}/g" "${folder}"/$wood/$recipe.json
-	 
+  # in the copied recipe, replace the string oak with the string for the input wood type 
+  
+  sed -i -e "s/oak/${wood}/g" "${folder}"/$wood/$recipe.json 
 	 
 done
-
