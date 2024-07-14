3a706badeb25465d913700f35582d9c043658f0b
lothrazar <samson.bassett@gmail.com>
2024-07-13 23:49:38 -0700

    3rd party compat for other wood in quark

--- scripts/forkRecipe.sh
+++ scripts/forkRecipe.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-[[ -z "$1" ]] && { echo "wood is empty for example: spruce" ; exit 1; }
+[[ -z "$1" ]] && { echo "wood is empty for example: ./forkRecipe.sh spruce (modid)" ; exit 1; }
 
 # assumes oak recipes are made, copies them to the destination wood type
 
 wood=$1
+seed="ancient" # was hardcoded to oak 
 
 modid="stonecutterlikeswood"
 folder="src/main/resources/data/${modid}/recipes"
@@ -14,7 +15,10 @@ mkdir -p "${folder}"/"${wood}"/
 
 # list all valid file recipes
 # TODO: improvement could be to loop on all files in /oak/ folder 
-declare -a arr=("boat" "button" "door" "fence" "gate" "hanging_sign" "log_sticks" "plate" "sign" "slab" "stairs" "stripped_log" "stripped_wood" "trapdoor" "wood_log")
+# 
+declare -a arr=("boat" "button" "door" "fence" "gate" "hanging_sign" "log_sticks" "plate" "sign" "slab" "stairs" "stripped_log" "stripped_wood" "trapdoor" "wood_log" 
+"slab_vertical" "post" "post_stripped"
+)
 arraylength=${#arr[@]}
 
 for (( i=0; i<${arraylength}; i++ ));
@@ -23,10 +27,19 @@ do
   
   # Copy the source oak recipe to the new input wood type
    
-  cp -rf "${folder}"/oak/"${recipe}".json "${folder}"/"${wood}"/$recipe.json
+  cp -rf "${folder}"/$seed/"${recipe}".json "${folder}"/"${wood}"/$recipe.json
 	
   # in the copied recipe, replace the string oak with the string for the input wood type 
   
-  sed -i -e "s/oak/${wood}/g" "${folder}"/$wood/$recipe.json 
+  sed -i -e "s/$seed/${wood}/g" "${folder}"/$wood/$recipe.json 
+  
+  if [[ ! -z "$2" ]] then
+	modid=$2
+	
+ 
+   sed -i -e "s/minecraft/${modid}/g" "${folder}"/$wood/$recipe.json 
+  
+  fi
+  
 	 
 done
