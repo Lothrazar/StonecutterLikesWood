74d62810ae9cfda0113e65e4bcdb8dcdd709e36c
Sam <lothrazar@hotmail.com>
2026-08-06 14:01:59 -0700

    porting 26.1 WIP

--- src/main/java/com/lothrazar/stonecutterlikeswood/ModRegistry.java
+++ src/main/java/com/lothrazar/stonecutterlikeswood/ModRegistry.java
@@ -1,6 +1,6 @@
 package com.lothrazar.stonecutterlikeswood;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -13,7 +13,7 @@ public class ModRegistry {
   public static void onRegistry(RegisterEvent event) {
     event.register(NeoForgeRegistries.Keys.CONDITION_CODECS,
         helper -> helper.register(
-            ResourceLocation.fromNamespaceAndPath(ModMain.MODID, "config"),
+            Identifier.fromNamespaceAndPath(ModMain.MODID, "config"),
             ConfigBooleanCondition.CODEC));
   }
 }
